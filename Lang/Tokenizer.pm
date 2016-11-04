f6ab226384441a95afbd5fe357d583011de96a71
mirror <mirror12k@gmail.com>
2016-11-04 01:23:31 -0400

    - imported tokenizer utility and iteration functions

--- Lang/Tokenizer.pm
+++ Lang/Tokenizer.pm
@@ -27,7 +27,7 @@ sub new {
 
 sub compile_tokenizer_regex {
 	my ($self) = @_;
-	my $token_pieces = join '|', map "(?<$_>$self->{token_regexes}{$_})", keys %{$self->{token_regexes}};
+	my $token_pieces = join '|', map "(?<" . $self->{token_regexes}[$_*2] . ">" . $self->{token_regexes}[$_*2+1] . ")", 0 .. $#{$self->{token_regexes}} / 2;
 	$self->{tokenizer_regex} = qr/$token_pieces/s;
 }
 
@@ -36,7 +36,7 @@ sub parse {
 
 	my $text;
 	$text = $self->{filepath}->read if defined $self->{filepath};
-	$text = $self->{text};
+	$text = $self->{text} unless defined $text;
 
 	croak "no text or filepath specified before parsing" unless defined $text;
 
@@ -53,7 +53,6 @@ sub parse_tokens {
 	my $line_number = 1;
 	my $offset = 0;
 
-	my @token_types = keys %{$self->{token_regexes}};
 	while ($text =~ /\G$self->{tokenizer_regex}/gc) {
 		my ($token_type, $token_text) = each %+;
 		push @tokens, [ $token_type => $token_text, $line_number, $offset ];
@@ -83,6 +82,117 @@ sub filter_tokens {
 	# do nothing; overridable optional method
 	return @tokens
 }
+
+
+
+
+
+
+sub more_tokens {
+	my ($self, $offset) = @_;
+	$offset //= 0;
+	return $self->{tokens_index} + $offset < @{$self->{tokens}}
+}
+
+sub peek_token {
+	my ($self) = @_;
+	return undef unless $self->more_tokens;
+	return $self->{tokens}[$self->{tokens_index}]
+}
+
+sub next_token {
+	my ($self) = @_;
+	return undef unless $self->more_tokens;
+	return $self->{tokens}[$self->{tokens_index}++]
+}
+
+
+
+sub is_token_type {
+	my ($self, $type, $offset) = @_;
+	return 0 unless $self->more_tokens;
+	return $self->{tokens}[$self->{tokens_index} + ($offset // 0)][0] eq $type
+}
+
+sub is_token_val {
+	my ($self, $type, $val, $offset) = @_;
+	return 0 unless $self->more_tokens;
+	return ($self->{tokens}[$self->{tokens_index} + ($offset // 0)][0] eq $type and
+			$self->{tokens}[$self->{tokens_index} + ($offset // 0)][1] eq $val)
+}
+
+sub assert_token_type {
+	my ($self, $type, $offset) = @_;
+	$self->confess_at_current_offset ("expected token type $type" . (defined $offset ? " (at offset $offset)" : '')
+			. " instead got token type $self->{tokens}[$self->{tokens_index}][0] with value $self->{tokens}[$self->{tokens_index}][1]")
+		unless $self->is_token_type($type, $offset);
+}
+
+sub assert_token_val {
+	my ($self, $type, $val, $offset) = @_;
+	$self->confess_at_current_offset ("expected token type $type with value '$val'" . (defined $offset ? " (at offset $offset)" : '')
+			. " instead got token type $self->{tokens}[$self->{tokens_index}][0] with value $self->{tokens}[$self->{tokens_index}][1]")
+		unless $self->is_token_val($type, $val, $offset);
+}
+
+sub assert_step_token_type {
+	my ($self, $type) = @_;
+	$self->assert_token_type($type);
+	return $self->next_token
+}
+
+sub assert_step_token_val {
+	my ($self, $type, $val) = @_;
+	$self->assert_token_val($type, $val);
+	return $self->next_token
+}
+
+sub confess_at_current_offset {
+	my ($self, $msg) = @_;
+
+	my $position;
+	if ($self->more_tokens) {
+		$position = 'line ' . $self->{tokens}[$self->{tokens_index}][2];
+	} else {
+		$position = 'end of file';
+	}
+
+	# say $self->dump_at_current_offset;
+
+	confess "error on $position: $msg";
+}
+
+sub current_line_number {
+	my ($self) = @_;
+	my $index = 0;
+	while ($self->more_tokens($index)) {
+		return $self->{tokens}[$self->{tokens_index} + $index][2] unless $self->is_token_type( whitespace => $index );
+		$index++;
+	}
+	return undef
+}
+
+
+sub dump {
+	my ($self) = @_;
+
+	return join "\n", map { "[$_->[2]:$_->[3]] $_->[0] => <$_->[1]>" } @{$self->{tokens}}
+}
+
+sub dump_at_current_offset {
+	my ($self) = @_;
+
+	my @tokens = @{$self->{tokens}};
+	return join "\n", map { "[$_->[2]:$_->[3]] $_->[0] => <$_->[1]>" } @tokens[$self->{tokens_index} .. $#tokens]
+}
+
+
+
+
+
+
+
+
 
 
 
